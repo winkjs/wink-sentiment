9e9dbde3c20666013bd52303dc2c735fc2268dce
sanjayaksaxena <sanjayaksaxena@yahoo.com>
2017-11-02 22:46:25 +0530

    chore(*): add license header

--- src/afinn-en-165-2grams.js
+++ src/afinn-en-165-2grams.js
@@ -1,3 +1,26 @@
+//     wink-sentiment
+//     Accurate and fast sentiment scoring of phrases with emoticons & emojis.
+//
+//     Copyright (C) 2017  GRAYPE Systems Private Limited
+//
+//     This file is part of “wink-sentiment”.
+//
+//     “wink-sentiment” is free software: you can redistribute
+//     it and/or modify it under the terms of the GNU Affero
+//     General Public License as published by the Free
+//     Software Foundation, version 3 of the License.
+//
+//     “wink-sentiment” is distributed in the hope that it will
+//     be useful, but WITHOUT ANY WARRANTY; without even
+//     the implied warranty of MERCHANTABILITY or FITNESS
+//     FOR A PARTICULAR PURPOSE.  See the GNU Affero General
+//     Public License for more details.
+//
+//     You should have received a copy of the GNU Affero
+//     General Public License along with “wink-sentiment”.
+//     If not, see <http://www.gnu.org/licenses/>.
+
+//
 // Each key in this object is an English word and the corresponding value is another
 // object; whose key is the next word that must be present in succession and its
 // value is the sentiment score of the 2-grams.
